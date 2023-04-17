6f05081f5ae96918451e67b62b8c67f7acb59715
Ashley W <39007548+AshleyW1@users.noreply.github.com>
2023-04-17 21:16:06 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.MessageContent
+  ],
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction]
+});
+
+client.once('ready', () => {
+
+    console.log(`Logged in as ${client.user.tag}.`);
+
+    Logging_Channel = client.channels.cache.get(`1097612437773095032`);
+    console.log(`Logging to ${Logging_Channel}`);
+
+
+  });
+
+  function log(message){
+    console.log(`~~\nAuthor: ${message.author.tag} - Channel: ${message.channel.name} - Timestamp: ${message.createdTimestamp} \nMessage: ${message.content}`)
+    
+    Logging_Channel = client.channels.cache.get(`1097612437773095032`);
+    Logging_Channel.send(`~~\nAuthor: ${message.author.tag} - Channel: ${message.channel.name} - Timestamp: ${message.createdTimestamp} \nMessage: ${message.content}`)
+  }
+
+  client.on("messageCreate", message => {
+    Logging_Channel = client.channels.cache.get(`1097612437773095032`);
+    
+    if (message.channelId == Logging_Channel){return null}
+    log(message)
+    if (message.author.bot){return null}
+
+    if (message.content.toLowerCase() === 'ping') {
+      message.channel.send(`${message.member} pong`)
+    }
+  });
+
+
+
+client.login("MTA5NzYwMjY2NjQ2NjY2MDQ3Mw.GwN2qc.x9sYeYAZ9EcGxf-tGNLwlNd50RwWNqSyF6DAwU")
